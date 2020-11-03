132bef5b5f1f2df3ef6efb20454ed4eec1992c1c
Rishabh Poddar <rishabh.poddar@gmail.com>
2020-11-03 20:51:30 +0530

    adds a few tests for signin

--- src/test/java/io/supertokens/test/EmailPasswordTest.java
+++ src/test/java/io/supertokens/test/EmailPasswordTest.java
@@ -18,6 +18,8 @@ package io.supertokens.test;
 
 import io.supertokens.ProcessState;
 import io.supertokens.emailpassword.EmailPassword;
+import io.supertokens.emailpassword.User;
+import io.supertokens.emailpassword.exceptions.WrongCredentialsException;
 import io.supertokens.pluginInterface.STORAGE_TYPE;
 import io.supertokens.pluginInterface.emailpassword.exceptions.DuplicateEmailException;
 import io.supertokens.pluginInterface.emailpassword.exceptions.DuplicateUserIdException;
@@ -38,10 +40,6 @@ import static org.junit.Assert.assertNotNull;
  *  - Test UpdatableBCrypt class
  *     - test time taken for hash
  *     - test hashing and verifying with short passwords and > 100 char password
- *  - Good input / output test for signUp
- *  - Good input / output test for signIn
- *  - Test wrong email provided to signIn
- *  - Test wrong password provided to signIn
  * */
 
 public class EmailPasswordTest {
@@ -134,4 +132,59 @@ public class EmailPasswordTest {
         process.kill();
         assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
     }
+
+    @Test
+    public void signUpAndThenSignIn() throws Exception {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args);
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorageLayer(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+
+        User userSignUp = EmailPassword.signUp(process.getProcess(), "test@example.com", "password");
+
+        User user = EmailPassword.signIn(process.getProcess(), "test@example.com", "password");
+
+        assert (user.email.equals("test@example.com"));
+
+        assert (userSignUp.id.equals(user.id));
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+    @Test
+    public void signInWrongEmailWrongPassword() throws Exception {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args);
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorageLayer(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+
+        try {
+            EmailPassword.signIn(process.getProcess(), "test@example.com", "password");
+            assert (false);
+        } catch (WrongCredentialsException e) {
+
+        }
+
+        EmailPassword.signUp(process.getProcess(), "test@example.com", "password");
+
+        try {
+            EmailPassword.signIn(process.getProcess(), "test@example.com", "password1");
+            assert (false);
+        } catch (WrongCredentialsException e) {
+
+        }
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
 }
