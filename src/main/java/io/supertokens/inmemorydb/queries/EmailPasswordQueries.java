d4af6fd15864b74b6569e6cb0b868056c804f91f
Rishabh Poddar <rishabh.poddar@gmail.com>
2021-01-08 10:13:50 +0530

    adds user pagination index

--- src/main/java/io/supertokens/inmemorydb/queries/EmailPasswordQueries.java
+++ src/main/java/io/supertokens/inmemorydb/queries/EmailPasswordQueries.java
@@ -42,6 +42,12 @@ public class EmailPasswordQueries {
                 + "is_email_verified INTEGER NOT NULL," + "PRIMARY KEY (user_id));";
     }
 
+    static String getQueryToCreateUserPaginationIndex(Start start) {
+        return "CREATE INDEX emailpassword_user_pagination_index ON " +
+                Config.getConfig(start).getUsersTable() + "(time_joined DESC, user_id " +
+                "DESC);";
+    }
+
     static String getQueryToCreatePasswordResetTokensTable(Start start) {
         return "CREATE TABLE IF NOT EXISTS " + Config.getConfig(start).getPasswordResetTokensTable() + " ("
                 + "user_id CHAR(36) NOT NULL," + "token VARCHAR(128) NOT NULL UNIQUE,"
