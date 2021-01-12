d303f0ac055f86e8829b5f21ff0be0e71a760701
Joel Coutinho <joelcoutinho@qually.io>
2021-01-12 18:01:15 +0530

    EmailPassword function tests

--- src/test/java/io/supertokens/test/EmailPasswordTest_2.java
+++ src/test/java/io/supertokens/test/EmailPasswordTest_2.java
@@ -19,6 +19,8 @@ package io.supertokens.test;
 import io.supertokens.ProcessState;
 import io.supertokens.emailpassword.EmailPassword;
 import io.supertokens.emailpassword.User;
+import io.supertokens.emailpassword.exceptions.EmailAlreadyVerifiedException;
+import io.supertokens.emailpassword.exceptions.EmailVerificationInvalidTokenException;
 import io.supertokens.pluginInterface.STORAGE_TYPE;
 import io.supertokens.pluginInterface.emailpassword.EmailVerificationTokenInfo;
 import io.supertokens.pluginInterface.emailpassword.exceptions.DuplicateEmailVerificationTokenException;
@@ -30,7 +32,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 /*
  * TODO:
@@ -58,6 +60,177 @@ public class EmailPasswordTest_2 {
     @Before
     public void beforeEach() {
         Utils.reset();
+    }
+
+    // Create an email verification token two times, and check that there are two entries in the db for that user with
+    // *  the right values
+    @Test
+    public void testGeneratingEmailVerificationTokenTwoTimes() throws Exception {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args);
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+
+        User user = EmailPassword.signUp(process.getProcess(), "test@example.com", "testPass123");
+        String token1 = EmailPassword.generateEmailVerificationToken(process.getProcess(), user.id);
+        String token2 = EmailPassword.generateEmailVerificationToken(process.getProcess(), user.id);
+
+        assertFalse(token1.equals(token2));
+
+        EmailVerificationTokenInfo[] tokenInfo = StorageLayer.getEmailPasswordStorage(process.getProcess())
+                .getAllEmailVerificationTokenInfoForUser(user.id);
+
+        assertEquals(tokenInfo.length, 2);
+        assertTrue((tokenInfo[0].token.equals(io.supertokens.utils.Utils.hashSHA256(token1))) ||
+                (tokenInfo[0].token.equals(io.supertokens.utils.Utils.hashSHA256(token2))));
+        assertTrue((tokenInfo[1].token.equals(io.supertokens.utils.Utils.hashSHA256(token1))) ||
+                (tokenInfo[1].token.equals(io.supertokens.utils.Utils.hashSHA256(token2))));
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+    // Verify the email successfully, then create an email verification token and check that the right error is thrown.
+    @Test
+    public void testVerifyingEmailAndGeneratingToken() throws Exception {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args);
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+
+        User user = EmailPassword.signUp(process.getProcess(), "test@example.com", "testPass123");
+        String token = EmailPassword.generateEmailVerificationToken(process.getProcess(), user.id);
+
+        EmailPassword.verifyEmail(process.getProcess(), token);
+        assertTrue(EmailPassword.isEmailVerified(process.getProcess(), user.id));
+
+        try {
+
+            EmailPassword.generateEmailVerificationToken(process.getProcess(), user.id);
+            throw new Exception("should not come here");
+        } catch (EmailAlreadyVerifiedException e) {
+        }
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+    // give invalid token to verify email
+    @Test
+    public void testInvalidTokenInputToVerifyEmail() throws Exception {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args);
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+
+        try {
+            EmailPassword.verifyEmail(process.getProcess(), "invalidToken");
+            throw new Exception("should not come here");
+        } catch (EmailVerificationInvalidTokenException e) {
+        }
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+    // Generate two tokens, verify with one token, the other token should throw an invalid token error
+    @Test
+    public void testGeneratingTwoTokenVerifyOtherTokenShouldThrowAnError() throws Exception {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args);
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+        User user = EmailPassword.signUp(process.getProcess(), "test@example.com", "testPass123");
+
+        String token1 = EmailPassword.generateEmailVerificationToken(process.getProcess(), user.id);
+        String token2 = EmailPassword.generateEmailVerificationToken(process.getProcess(), user.id);
+
+        EmailPassword.verifyEmail(process.getProcess(), token1);
+        assertTrue(EmailPassword.isEmailVerified(process.getProcess(), user.id));
+
+        try {
+            EmailPassword.verifyEmail(process.getProcess(), token2);
+            throw new Exception("should not come here");
+        } catch (EmailVerificationInvalidTokenException e) {
+
+        }
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+    // Use an expired token, it should throw an error
+    @Test
+    public void useAnExpiredTokenItShouldThrowAnError() throws Exception {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args, false);
+
+        io.supertokens.emailpassword.EmailPasswordTest.getInstance(process.getProcess())
+                .setEmailVerificationTokenLifetime(10);
+
+        process.startProcess();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+        User user = EmailPassword.signUp(process.getProcess(), "test@example.com", "testPass123");
+
+        String token = EmailPassword.generateEmailVerificationToken(process.getProcess(), user.id);
+
+        Thread.sleep(20);
+
+        try {
+            EmailPassword.verifyEmail(process.getProcess(), token);
+            throw new Exception("should not come here");
+        } catch (EmailVerificationInvalidTokenException e) {
+
+        }
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+    // Test the format of the email verification token
+    @Test
+    public void testFormatOfEmailVerificationToken() throws Exception {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args);
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+        User user = EmailPassword.signUp(process.getProcess(), "test@example.com", "testPass123");
+
+        for (int i = 0; i < 100; i++) {
+            String verifyToken = EmailPassword.generatePasswordResetToken(process.getProcess(), user.id);
+            assertEquals(verifyToken.length(), 128);
+            assertFalse(verifyToken.contains("+"));
+            assertFalse(verifyToken.contains("="));
+            assertFalse(verifyToken.contains("/"));
+        }
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
     }
 
     @Test
