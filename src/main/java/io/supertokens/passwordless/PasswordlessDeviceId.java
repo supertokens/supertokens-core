8fd5c9620068d207aa228d19c6486fde28a26f62
Mihaly Lengyel <mihaly.lengyel@tresorit.com>
2021-12-20 12:46:51 +0100

    Extended explanation comment for linkCodeSalt

--- src/main/java/io/supertokens/passwordless/PasswordlessDeviceId.java
+++ src/main/java/io/supertokens/passwordless/PasswordlessDeviceId.java
@@ -29,8 +29,14 @@ public class PasswordlessDeviceId {
 
     public PasswordlessLinkCode getLinkCode(PasswordlessLinkCodeSalt linkCodeSalt, String userInputCode)
             throws NoSuchAlgorithmException, InvalidKeyException, IOException {
-        // We mix in the salt into the deviceId by concatenating them.
-        // HMAC-SHA256 takes 64 byte keys by default (and would hash them otherwise to get 64 bytes)
+        // We need the linkCodeSalt while calculating the linkCode because we do not want it to be calculatable on the
+        // client-side. If it were, then an attacker could try to guess the userInputCode based on the received deviceId
+        // and circumvent the failed attempt count limit by calling the consume endpoint with a linkCode.
+        // Since we assume the userInputCode to be low entropy, it's possible it could be brute-forced without setting
+        // low limits on the retry count.
+
+        // We mix the salt into the deviceId by concatenating them.
+        // HMAC-SHA256 takes 64-byte keys by default (and would hash them otherwise to get 64 bytes)
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         outputStream.write(bytes);
         outputStream.write(linkCodeSalt.bytes);
