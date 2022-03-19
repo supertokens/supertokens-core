ebbcad67e3c3aa372e739d7d9d1a445d94d0c316
Rishabh <rishabh@supertokens.io>
2022-03-19 23:29:39 +0530

    update to action script

--- .github/helpers/test-pass-check-pr.js
+++ .github/helpers/test-pass-check-pr.js
@@ -5,5 +5,5 @@ const currentCommitHash = process.env.COMMIT_HASH;
 const githubURL = `https://api.github.com/repos/${process.env.REPO}/actions/runs?branch=${process.env.BRANCH}`
 
 axios.get(githubURL).then(result => {
-    console.log(result);
+    console.log(result.data);
 })
