7693759f0db70c34d8b4198e8742b756379b6097
Rishabh <rishabh@supertokens.io>
2022-03-20 00:29:03 +0530

    update to action script

--- .github/helpers/test-pass-check-pr.js
+++ .github/helpers/test-pass-check-pr.js
@@ -16,14 +16,15 @@ axios.get(`https://api.github.com/repos/${process.env.REPO}/actions/runs?branch=
 
     if (currentSHA !== undefined) {
         for (let i = 0; i < data.workflow_runs.length; i++) {
-            console.log("I!!!!!!", i);
             let run = data.workflow_runs[i];
             if (run.head_sha === currentSHA) {
                 // here we have all the jobs that have run on this commit.
                 let workflow_id = run.workflow_id;
                 let workflow = await axios.get(`https://api.github.com/repos/${process.env.REPO}/actions/workflows/${workflow_id}`);
                 let workflowData = workflow.data;
-                console.log(workflowData);
+                if (workflowData.path === ".github/workflows/tests.yml") {
+                    console.log(workflowData);
+                }
             }
         }
     }
