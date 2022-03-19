07a8a77117d5b61984405deaaaa5c890766a9d17
Rishabh <rishabh@supertokens.io>
2022-03-20 00:01:08 +0530

    update to action script

--- .github/helpers/test-pass-check-pr.js
+++ .github/helpers/test-pass-check-pr.js
@@ -1,20 +1,21 @@
 const { default: axios } = require("axios");
 
 console.log("Hi from node!!", process.env.COMMIT_HASH);
-const currentCommitHash = process.env.COMMIT_HASH;
+const thisJobId = process.env.JOB_ID;
 const githubURL = `https://api.github.com/repos/${process.env.REPO}/actions/runs?branch=${process.env.BRANCH}`
 
 axios.get(githubURL).then(result => {
     let data = result.data;
     let passed = false;
+    let currentSHA = "";
 
     data.workflow_runs.forEach(run => {
-        if (run.head_sha === currentCommitHash) {
-            console.log(run);
-        } else {
-            console.log("not matched", run.head_sha, currentCommitHash)
+        if (run.id === thisJobId) {
+            currentSHA = run.head_sha;
         }
     });
+
+    console.log(currentSHA);
 
     process.exitCode(passed ? 0 : 1);
 })
