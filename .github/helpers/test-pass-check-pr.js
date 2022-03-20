adbba7e34398f663c3bc089d7c576511f13f0c13
Rishabh <rishabh@supertokens.io>
2022-03-20 20:26:38 +0530

    script change

--- .github/helpers/test-pass-check-pr.js
+++ .github/helpers/test-pass-check-pr.js
@@ -1,5 +1,6 @@
 const { default: axios } = require("axios");
 
+const readmeInstrsLink = "https://github.com/supertokens/supertokens-core/blob/master/CONTRIBUTING.md#using-github-actions";
 let thisRunId = process.env.RUN_ID;
 thisRunId = thisRunId.trim();
 
@@ -7,7 +8,6 @@ function doJob() {
     console.log("Trying to check test job status...");
     axios.get(`https://api.github.com/repos/${process.env.REPO}/actions/runs?branch=${process.env.BRANCH}`).then(async result => {
         let data = result.data;
-        let passed = false;
         let currentSHA = undefined;
 
         data.workflow_runs.forEach(run => {
@@ -16,6 +16,7 @@ function doJob() {
             }
         });
 
+        let foundJob = false;
         if (currentSHA !== undefined) {
             for (let i = 0; i < data.workflow_runs.length; i++) {
                 let run = data.workflow_runs[i];
@@ -25,22 +26,26 @@ function doJob() {
                     let workflow = await axios.get(`https://api.github.com/repos/${process.env.REPO}/actions/workflows/${workflow_id}`);
                     let workflowData = workflow.data;
                     if (workflowData.path === ".github/workflows/tests.yml") {
+                        foundJob = true;
                         if (run.conclusion === "success") {
-                            passed = true;
+                            console.log("Success!");
+                            process.exit(0);
                             break;
+                        } else if (run.conclusion === "failure") {
+                            console.log("Test job failed... exiting");
+                            process.exit(1);
                         }
                     }
                 }
             }
         }
 
-        if (!passed) {
-            console.log("You need to trigger the \"Run tests\" github action and make that succeed.\nSee https://github.com/supertokens/supertokens-core/blob/master/CONTRIBUTING.md#using-github-actions")
-
-            setTimeout(doJob, 30000) // try again after 30 seconds.
+        if (!foundJob) {
+            console.log("You need to trigger the \"Run tests\" github action and make that succeed.\nSee " + readmeInstrsLink);
         } else {
-            process.exit(0);
+            console.log("Waiting for test job to finish...");
         }
+        setTimeout(doJob, 30000) // try again after 30 seconds.
     }).catch(() => {
         console.log("Error thrown.. waiting for 1 min and trying again.");
         setTimeout(doJob, 60000) // try again after 1 min.
