a747ed3594a87b41a711200279d79e6d5f033ca0
jscyo <joelsteve97@hotmail.com>
2022-07-14 12:01:33 +0530

    temp commit

--- src/main/java/io/supertokens/webserver/Webserver.java
+++ src/main/java/io/supertokens/webserver/Webserver.java
@@ -38,7 +38,7 @@ import io.supertokens.webserver.api.passwordless.*;
 import io.supertokens.webserver.api.session.*;
 import io.supertokens.webserver.api.thirdparty.GetUsersByEmailAPI;
 import io.supertokens.webserver.api.thirdparty.SignInUpAPI;
-import io.supertokens.webserver.api.useridmapping.CreateUserIdMappingAPI;
+import io.supertokens.webserver.api.useridmapping.UserIdMappingAPI;
 import io.supertokens.webserver.api.usermetadata.UserMetadataAPI;
 import io.supertokens.webserver.api.usermetadata.RemoveUserMetadataAPI;
 
@@ -202,7 +202,7 @@ public class Webserver extends ResourceDistributor.SingletonResource {
         addAPI(new GetRolesForPermissionAPI(main));
         addAPI(new RemoveRoleAPI(main));
         addAPI(new GetRolesAPI(main));
-        addAPI(new CreateUserIdMappingAPI(main));
+        addAPI(new UserIdMappingAPI(main));
         // deprecated APIs:
         addAPI(new RecipeRouter(main, new io.supertokens.webserver.api.emailpassword.UsersAPI(main),
                 new io.supertokens.webserver.api.thirdparty.UsersAPI(main)));
