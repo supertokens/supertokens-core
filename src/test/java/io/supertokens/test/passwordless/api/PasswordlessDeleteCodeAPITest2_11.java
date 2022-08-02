7d4c7cd7f2654115c4810ca5815ed9ce85f4977a
Anugrah Singhal <anugrahsinghal1@gmail.com>
2022-08-02 15:33:46 +0530

    fix: code id generation for PasswordlessCode Test (#490)
    * fix: code id generation for Passwordless
    
    * chore: adds changelog

--- src/test/java/io/supertokens/test/passwordless/api/PasswordlessDeleteCodeAPITest2_11.java
+++ src/test/java/io/supertokens/test/passwordless/api/PasswordlessDeleteCodeAPITest2_11.java
@@ -63,7 +63,7 @@ public class PasswordlessDeleteCodeAPITest2_11 {
         PasswordlessSQLStorage storage = StorageLayer.getPasswordlessStorage(process.getProcess());
 
         String phoneNumber = "+442071838750";
-        String codeId = "codeId";
+        String codeId = io.supertokens.utils.Utils.getUUID();
 
         String deviceIdHash = "pZ9SP0USbXbejGFO6qx7x3JBjupJZVtw4RkFiNtJGqc";
         String linkCodeHash = "wo5UcFFVSblZEd1KOUOl-dpJ5zpSr_Qsor1Eg4TzDRE";
