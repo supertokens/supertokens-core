7aeba0c388bf282aba6ff8c13e424054c5113091
jscyo <joelsteve97@hotmail.com>
2022-08-09 14:00:43 +0530

    adds more tests

--- src/test/java/io/supertokens/test/userIdMapping/recipe/PasswordlessAPITest.java
+++ src/test/java/io/supertokens/test/userIdMapping/recipe/PasswordlessAPITest.java
@@ -16,6 +16,7 @@
 
 package io.supertokens.test.userIdMapping.recipe;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import io.supertokens.ProcessState;
 import io.supertokens.authRecipe.AuthRecipe;
@@ -33,6 +34,8 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
+
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
@@ -115,4 +118,58 @@ public class PasswordlessAPITest {
         process.kill();
         assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
     }
+
+    // getUserAPI tests
+    @Test
+    public void testCreatingAPasswordlessUserAndRetrievingInfo() throws Exception {
+        String[] args = { "../" };
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args);
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+
+        String externalId = "externalId";
+        String superTokensUserId;
+        String email = "test@example.com";
+        // create a passwordless User
+        {
+            Passwordless.CreateCodeResponse createCodeResponse = Passwordless.createCode(process.main, email, null,
+                    null, null);
+            Passwordless.ConsumeCodeResponse consumeCodeResponse = Passwordless.consumeCode(process.main,
+                    createCodeResponse.deviceId, createCodeResponse.deviceIdHash, createCodeResponse.userInputCode,
+                    null);
+            assertTrue(consumeCodeResponse.createdNewUser);
+            superTokensUserId = consumeCodeResponse.user.id;
+
+            // create mapping
+            UserIdMapping.createUserIdMapping(process.main, superTokensUserId, externalId, null);
+
+            // check that mapping exists
+            io.supertokens.pluginInterface.useridmapping.UserIdMapping response = UserIdMapping
+                    .getUserIdMapping(process.main, superTokensUserId, UserIdType.SUPERTOKENS);
+            assertNotNull(response);
+            assertEquals(response.superTokensUserId, superTokensUserId);
+            assertEquals(response.externalUserId, externalId);
+        }
+
+        {
+            // retrieving UserInfo with email
+            HashMap<String, String> query = new HashMap<>();
+            query.put("email", email);
+
+            JsonObject response = HttpRequestForTesting.sendGETRequest(process.getProcess(), "",
+                    "http://localhost:3567/recipe/user", query, 1000, 1000, null, Utils.getCdiVersion2_15ForTests(),
+                    "passwordless");
+            assertEquals("OK", response.get("status").getAsString());
+            assertEquals(externalId, response.get("user").getAsJsonObject().get("id").getAsString());
+
+        }
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
 }
