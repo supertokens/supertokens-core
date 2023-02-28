d2555139857ca4500d71831ac1b17b4e713f9223
Sattvik Chakravarthy <sattvik@gmail.com>
2023-02-28 13:29:54 +0530

    fix: new tests

--- src/test/java/io/supertokens/test/multitenant/StorageLayerTest.java
+++ src/test/java/io/supertokens/test/multitenant/StorageLayerTest.java
@@ -829,6 +829,268 @@ public class StorageLayerTest {
     }
 
     @Test
+    public void testCreateDuplicateTenantWithDuplicateProviderIdThrowsDuplicateTenantException()
+            throws InterruptedException, StorageQueryException, DuplicateClientTypeException,
+            DuplicateThirdPartyIdException {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args, false);
+        FeatureFlagTestContent.getInstance(process.getProcess())
+                .setKeyValue(FeatureFlagTestContent.ENABLED_FEATURES, new EE_FEATURES[]{EE_FEATURES.MULTI_TENANCY});
+        process.startProcess();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.LOADING_ALL_TENANT_STORAGE));
+        MultitenancyStorage mtStorage = StorageLayer.getMultitenancyStorage(process.getProcess());
+
+        JsonObject authParams = new JsonObject();
+        authParams.add("auth-param", new JsonPrimitive("auth-val"));
+
+        JsonObject tokenParams = new JsonObject();
+        tokenParams.add("token-param", new JsonPrimitive("token-val"));
+
+        JsonObject userInfoParams = new JsonObject();
+        userInfoParams.add("user-param", new JsonPrimitive("user-val"));
+
+        JsonObject userInfoHeaders = new JsonObject();
+        userInfoHeaders.add("user-header", new JsonPrimitive("user-header-val"));
+        try {
+            mtStorage.createTenant(new TenantConfig(
+                    new TenantIdentifier(null, null, "t1"),
+                    new EmailPasswordConfig(true),
+                    new ThirdPartyConfig(true, new ThirdPartyConfig.Provider[]{
+                            new ThirdPartyConfig.Provider(
+                                    "google",
+                                    "Google",
+                                    new ThirdPartyConfig.ProviderClient[]{
+                                            new ThirdPartyConfig.ProviderClient(
+                                                    "web",
+                                                    "client-id",
+                                                    "client-secret",
+                                                    new String[]{"scope-1", "scope-2"},
+                                                    true,
+                                                    new JsonObject()
+                                            )
+                                    },
+                                    "https://auth.example.com/auth",
+                                    authParams,
+                                    "https://auth.example.com/token",
+                                    tokenParams,
+                                    "https://auth.example.com/user",
+                                    userInfoParams,
+                                    userInfoHeaders,
+                                    "https://auth.example.com/jwks",
+                                    "https://auth.example.com",
+                                    true,
+                                    new ThirdPartyConfig.UserInfoMap(
+                                            new ThirdPartyConfig.UserInfoMapKeyValue("id1", "email1", "email_verified1"),
+                                            new ThirdPartyConfig.UserInfoMapKeyValue("id2", "email2", "email_verified2")
+                                    )
+                            )
+                    }),
+                    new PasswordlessConfig(true),
+                    new JsonObject()
+            ));
+        } catch (DuplicateTenantException e) {
+            fail();
+        }
+
+        try {
+            mtStorage.createTenant(new TenantConfig(
+                    new TenantIdentifier(null, null, "t1"),
+                    new EmailPasswordConfig(true),
+                    new ThirdPartyConfig(true, new ThirdPartyConfig.Provider[]{
+                            new ThirdPartyConfig.Provider(
+                                    "google",
+                                    "Google",
+                                    new ThirdPartyConfig.ProviderClient[]{
+                                            new ThirdPartyConfig.ProviderClient(
+                                                    "web",
+                                                    "client-id",
+                                                    "client-secret",
+                                                    new String[]{"scope-1", "scope-2"},
+                                                    true,
+                                                    new JsonObject()
+                                            )
+                                    },
+                                    "https://auth.example.com/auth",
+                                    authParams,
+                                    "https://auth.example.com/token",
+                                    tokenParams,
+                                    "https://auth.example.com/user",
+                                    userInfoParams,
+                                    userInfoHeaders,
+                                    "https://auth.example.com/jwks",
+                                    "https://auth.example.com",
+                                    true,
+                                    new ThirdPartyConfig.UserInfoMap(
+                                            new ThirdPartyConfig.UserInfoMapKeyValue("id1", "email1", "email_verified1"),
+                                            new ThirdPartyConfig.UserInfoMapKeyValue("id2", "email2", "email_verified2")
+                                    )
+                            ),
+                            new ThirdPartyConfig.Provider(
+                                    "google",
+                                    "Google",
+                                    new ThirdPartyConfig.ProviderClient[]{
+                                            new ThirdPartyConfig.ProviderClient(
+                                                    "web",
+                                                    "client-id",
+                                                    "client-secret",
+                                                    new String[]{"scope-1", "scope-2"},
+                                                    true,
+                                                    new JsonObject()
+                                            )
+                                    },
+                                    "https://auth.example.com/auth",
+                                    authParams,
+                                    "https://auth.example.com/token",
+                                    tokenParams,
+                                    "https://auth.example.com/user",
+                                    userInfoParams,
+                                    userInfoHeaders,
+                                    "https://auth.example.com/jwks",
+                                    "https://auth.example.com",
+                                    true,
+                                    new ThirdPartyConfig.UserInfoMap(
+                                            new ThirdPartyConfig.UserInfoMapKeyValue("id1", "email1", "email_verified1"),
+                                            new ThirdPartyConfig.UserInfoMapKeyValue("id2", "email2", "email_verified2")
+                                    )
+                            )
+                    }),
+                    new PasswordlessConfig(true),
+                    new JsonObject()
+            ));
+            fail();
+        } catch (DuplicateTenantException e) {
+            // pass
+        }
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+    @Test
+    public void testCreateDuplicateTenantWithDuplicateProviderClientTypeThrowsDuplicateTenantException()
+            throws InterruptedException, StorageQueryException, DuplicateClientTypeException,
+            DuplicateThirdPartyIdException {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args, false);
+        FeatureFlagTestContent.getInstance(process.getProcess())
+                .setKeyValue(FeatureFlagTestContent.ENABLED_FEATURES, new EE_FEATURES[]{EE_FEATURES.MULTI_TENANCY});
+        process.startProcess();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.LOADING_ALL_TENANT_STORAGE));
+        MultitenancyStorage mtStorage = StorageLayer.getMultitenancyStorage(process.getProcess());
+
+        JsonObject authParams = new JsonObject();
+        authParams.add("auth-param", new JsonPrimitive("auth-val"));
+
+        JsonObject tokenParams = new JsonObject();
+        tokenParams.add("token-param", new JsonPrimitive("token-val"));
+
+        JsonObject userInfoParams = new JsonObject();
+        userInfoParams.add("user-param", new JsonPrimitive("user-val"));
+
+        JsonObject userInfoHeaders = new JsonObject();
+        userInfoHeaders.add("user-header", new JsonPrimitive("user-header-val"));
+        try {
+            mtStorage.createTenant(new TenantConfig(
+                    new TenantIdentifier(null, null, "t1"),
+                    new EmailPasswordConfig(true),
+                    new ThirdPartyConfig(true, new ThirdPartyConfig.Provider[]{
+                            new ThirdPartyConfig.Provider(
+                                    "google",
+                                    "Google",
+                                    new ThirdPartyConfig.ProviderClient[]{
+                                            new ThirdPartyConfig.ProviderClient(
+                                                    "web",
+                                                    "client-id",
+                                                    "client-secret",
+                                                    new String[]{"scope-1", "scope-2"},
+                                                    true,
+                                                    new JsonObject()
+                                            )
+                                    },
+                                    "https://auth.example.com/auth",
+                                    authParams,
+                                    "https://auth.example.com/token",
+                                    tokenParams,
+                                    "https://auth.example.com/user",
+                                    userInfoParams,
+                                    userInfoHeaders,
+                                    "https://auth.example.com/jwks",
+                                    "https://auth.example.com",
+                                    true,
+                                    new ThirdPartyConfig.UserInfoMap(
+                                            new ThirdPartyConfig.UserInfoMapKeyValue("id1", "email1", "email_verified1"),
+                                            new ThirdPartyConfig.UserInfoMapKeyValue("id2", "email2", "email_verified2")
+                                    )
+                            )
+                    }),
+                    new PasswordlessConfig(true),
+                    new JsonObject()
+            ));
+        } catch (DuplicateTenantException e) {
+            fail();
+        }
+
+        try {
+            mtStorage.createTenant(new TenantConfig(
+                    new TenantIdentifier(null, null, "t1"),
+                    new EmailPasswordConfig(true),
+                    new ThirdPartyConfig(true, new ThirdPartyConfig.Provider[]{
+                            new ThirdPartyConfig.Provider(
+                                    "google",
+                                    "Google",
+                                    new ThirdPartyConfig.ProviderClient[]{
+                                            new ThirdPartyConfig.ProviderClient(
+                                                    "web",
+                                                    "client-id",
+                                                    "client-secret",
+                                                    new String[]{"scope-1", "scope-2"},
+                                                    true,
+                                                    new JsonObject()
+                                            ),
+                                            new ThirdPartyConfig.ProviderClient(
+                                                    "web",
+                                                    "client-id",
+                                                    "client-secret",
+                                                    new String[]{"scope-1", "scope-2"},
+                                                    true,
+                                                    new JsonObject()
+                                            )
+                                    },
+                                    "https://auth.example.com/auth",
+                                    authParams,
+                                    "https://auth.example.com/token",
+                                    tokenParams,
+                                    "https://auth.example.com/user",
+                                    userInfoParams,
+                                    userInfoHeaders,
+                                    "https://auth.example.com/jwks",
+                                    "https://auth.example.com",
+                                    true,
+                                    new ThirdPartyConfig.UserInfoMap(
+                                            new ThirdPartyConfig.UserInfoMapKeyValue("id1", "email1", "email_verified1"),
+                                            new ThirdPartyConfig.UserInfoMapKeyValue("id2", "email2", "email_verified2")
+                                    )
+                            )
+                    }),
+                    new PasswordlessConfig(true),
+                    new JsonObject()
+            ));
+            fail();
+        } catch (DuplicateTenantException e) {
+            // pass
+        }
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+    @Test
     public void testCreateTenantWithDuplicateClientTypeThrowsException()
             throws DuplicateTenantException, InterruptedException, StorageQueryException,
             DuplicateThirdPartyIdException {
