67c8a03a07405a5b452b28ad1596f27ceeea27ed
Sattvik Chakravarthy <sattvik@gmail.com>
2023-02-28 16:01:59 +0530

    fix: concurrent test

--- src/test/java/io/supertokens/test/multitenant/StorageLayerTest.java
+++ src/test/java/io/supertokens/test/multitenant/StorageLayerTest.java
@@ -35,6 +35,10 @@ import org.junit.*;
 import org.junit.rules.TestRule;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.junit.Assert.*;
 
@@ -1534,6 +1538,137 @@ public class StorageLayerTest {
         } catch (DuplicateClientTypeException e) {
             // Pass
         }
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+    @Test
+    public void testOverwriteTenantForRaceConditions()
+            throws StorageQueryException, InterruptedException {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args, false);
+        FeatureFlagTestContent.getInstance(process.getProcess())
+                .setKeyValue(FeatureFlagTestContent.ENABLED_FEATURES, new EE_FEATURES[]{EE_FEATURES.MULTI_TENANCY});
+        process.startProcess();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.LOADING_ALL_TENANT_STORAGE));
+        MultitenancyStorage mtStorage = StorageLayer.getMultitenancyStorage(process.getProcess());
+
+        JsonObject authParams = new JsonObject();
+        authParams.add("auth-param", new JsonPrimitive("auth-val"));
+
+        JsonObject tokenParams = new JsonObject();
+        tokenParams.add("token-param", new JsonPrimitive("token-val"));
+
+        JsonObject userInfoParams = new JsonObject();
+        userInfoParams.add("user-param", new JsonPrimitive("user-val"));
+
+        JsonObject userInfoHeaders = new JsonObject();
+        userInfoHeaders.add("user-header", new JsonPrimitive("user-header-val"));
+
+        ExecutorService es = Executors.newFixedThreadPool(1000);
+
+        AtomicBoolean pass = new AtomicBoolean(true);
+
+        for (int i = 0; i < 3000; i++) {
+            es.execute(() -> {
+                while (true) {
+                    try {
+                        mtStorage.overwriteTenantConfig(new TenantConfig(
+                                new TenantIdentifier(null, null, null),
+                                new EmailPasswordConfig(true),
+                                new ThirdPartyConfig(true, new ThirdPartyConfig.Provider[]{
+                                        new ThirdPartyConfig.Provider(
+                                                "google",
+                                                "Google",
+                                                new ThirdPartyConfig.ProviderClient[]{
+                                                        new ThirdPartyConfig.ProviderClient(
+                                                                "web",
+                                                                "client-id",
+                                                                "client-secret",
+                                                                new String[]{"scope-1", "scope-2"},
+                                                                true,
+                                                                new JsonObject()
+                                                        )
+                                                },
+                                                "https://auth.example.com/auth",
+                                                authParams,
+                                                "https://auth.example.com/token",
+                                                tokenParams,
+                                                "https://auth.example.com/user",
+                                                userInfoParams,
+                                                userInfoHeaders,
+                                                "https://auth.example.com/jwks",
+                                                "https://auth.example.com",
+                                                true,
+                                                new ThirdPartyConfig.UserInfoMap(
+                                                        new ThirdPartyConfig.UserInfoMapKeyValue("id1", "email1",
+                                                                "email_verified1"),
+                                                        new ThirdPartyConfig.UserInfoMapKeyValue("id2", "email2",
+                                                                "email_verified2")
+                                                )
+                                        )
+                                }),
+                                new PasswordlessConfig(true),
+                                new JsonObject()
+                        ));
+                        break;
+                    } catch (Exception e) {
+                        if (e instanceof StorageQueryException) {
+                            if (e.getMessage().contains("request timed out") || e.getMessage().contains("concurrent delete")) {
+                                // retry, because connection was timed out, or
+                                // in case of postgres, number of retries may not be enough, we retry here anyway
+                                continue;
+                            }
+                        }
+                        System.out.println(e.getMessage());
+                        pass.set(false);
+                        break;
+                    }
+                }
+            });
+        }
+
+        es.shutdown();
+        es.awaitTermination(2, TimeUnit.MINUTES);
+
+        assert (pass.get());
+
+        TenantConfig[] tenantConfigs = mtStorage.getAllTenants();
+
+        assertEquals(1, tenantConfigs.length);
+        TenantConfig baseTenantConfig = tenantConfigs[0];
+
+        assertEquals(new TenantIdentifier(null, null, null), baseTenantConfig.tenantIdentifier);
+        assertTrue(baseTenantConfig.emailPasswordConfig.enabled);
+        assertTrue(baseTenantConfig.passwordlessConfig.enabled);
+        assertTrue(baseTenantConfig.thirdPartyConfig.enabled);
+        assertNotNull(baseTenantConfig.thirdPartyConfig.providers);
+        assertEquals(1, baseTenantConfig.thirdPartyConfig.providers.length);
+
+        ThirdPartyConfig.Provider provider = baseTenantConfig.thirdPartyConfig.providers[0];
+
+        assertEquals("google", provider.thirdPartyId);
+        assertEquals("Google", provider.name);
+        assertEquals("https://auth.example.com/auth", provider.authorizationEndpoint);
+        assertEquals(authParams, provider.authorizationEndpointQueryParams);
+        assertEquals("https://auth.example.com/token", provider.tokenEndpoint);
+        assertEquals(tokenParams, provider.tokenEndpointBodyParams);
+        assertEquals("https://auth.example.com/user", provider.userInfoEndpoint);
+        assertEquals(userInfoParams, provider.userInfoEndpointQueryParams);
+        assertEquals(userInfoHeaders, provider.userInfoEndpointHeaders);
+        assertEquals("https://auth.example.com/jwks", provider.jwksURI);
+        assertEquals("https://auth.example.com", provider.oidcDiscoveryEndpoint);
+        assertTrue(provider.requireEmail);
+        assertEquals("id1", provider.userInfoMap.fromIdTokenPayload.userId);
+        assertEquals("email1", provider.userInfoMap.fromIdTokenPayload.email);
+        assertEquals("email_verified1", provider.userInfoMap.fromIdTokenPayload.emailVerified);
+        assertEquals("id2", provider.userInfoMap.fromUserInfoAPI.userId);
+        assertEquals("email2", provider.userInfoMap.fromUserInfoAPI.email);
+        assertEquals("email_verified2", provider.userInfoMap.fromUserInfoAPI.emailVerified);
 
         process.kill();
         assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
