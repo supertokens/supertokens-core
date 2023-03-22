f45edf267c90bafacff91c5af7b977e06ea940b0
KShivendu <shivendu@iitbhilai.ac.in>
2023-03-22 12:10:17 +0530

    fix: Suppress usermapping exceptions for active users monitoring

--- src/main/java/io/supertokens/webserver/api/session/RefreshSessionAPI.java
+++ src/main/java/io/supertokens/webserver/api/session/RefreshSessionAPI.java
@@ -64,13 +64,17 @@ public class RefreshSessionAPI extends WebserverAPI {
             SessionInformationHolder sessionInfo = Session.refreshSession(main, refreshToken, antiCsrfToken,
                     enableAntiCsrf);
 
-            UserIdMapping userIdMapping = io.supertokens.useridmapping.UserIdMapping.getUserIdMapping(super.main,
-                    sessionInfo.session.userId, UserIdType.ANY);
-            if (userIdMapping != null) {
-                ActiveUsers.updateLastActive(main, userIdMapping.superTokensUserId);
-            } else {
-                ActiveUsers.updateLastActive(main, sessionInfo.session.userId);
+            try{
+                UserIdMapping userIdMapping = io.supertokens.useridmapping.UserIdMapping.getUserIdMapping(super.main,
+                        sessionInfo.session.userId, UserIdType.ANY);
+                if (userIdMapping != null) {
+                    ActiveUsers.updateLastActive(main, userIdMapping.superTokensUserId);
+                } else {
+                    ActiveUsers.updateLastActive(main, sessionInfo.session.userId);
+                }
+            } catch (StorageQueryException ignored){
             }
+
 
             JsonObject result = sessionInfo.toJsonObject();
             result.addProperty("status", "OK");
