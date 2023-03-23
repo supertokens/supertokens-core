90f1d0b82ac342a931d5637ac6bfd576c17c8194
KShivendu <shivendu@iitbhilai.ac.in>
2023-03-23 17:13:07 +0530

    refactor: Use internal supertokens user id in TOTP APIs

--- src/main/java/io/supertokens/webserver/api/totp/VerifyTotpAPI.java
+++ src/main/java/io/supertokens/webserver/api/totp/VerifyTotpAPI.java
@@ -9,9 +9,11 @@ import io.supertokens.pluginInterface.RECIPE_ID;
 import io.supertokens.pluginInterface.exceptions.StorageQueryException;
 import io.supertokens.pluginInterface.exceptions.StorageTransactionLogicException;
 import io.supertokens.pluginInterface.totp.exception.TotpNotEnabledException;
+import io.supertokens.pluginInterface.useridmapping.UserIdMapping;
 import io.supertokens.totp.Totp;
 import io.supertokens.totp.exceptions.InvalidTotpException;
 import io.supertokens.totp.exceptions.LimitReachedException;
+import io.supertokens.useridmapping.UserIdType;
 import io.supertokens.webserver.InputParser;
 import io.supertokens.webserver.WebserverAPI;
 import jakarta.servlet.ServletException;
@@ -49,6 +51,11 @@ public class VerifyTotpAPI extends WebserverAPI {
         JsonObject result = new JsonObject();
 
         try {
+            UserIdMapping userIdMapping = io.supertokens.useridmapping.UserIdMapping.getUserIdMapping(super.main, userId, UserIdType.ANY);
+            if (userIdMapping != null) {
+                userId = userIdMapping.superTokensUserId;
+            }
+
             Totp.verifyCode(main, userId, totp, allowUnverifiedDevices);
 
             result.addProperty("status", "OK");
