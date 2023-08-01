06a1969271a916f4b75a2aaca28b271202cc6fc5
Sattvik Chakravarthy <sattvik@supertokens.com>
2023-08-01 17:35:17 +0530

    fix: cud validation (#761)

--- src/main/java/io/supertokens/webserver/Utils.java
+++ src/main/java/io/supertokens/webserver/Utils.java
@@ -47,7 +47,7 @@ public class Utils {
             throw new ServletException(new WebserverAPI.BadRequestException("connectionUriDomain should not be an empty String"));
         }
 
-        String hostnameRegex = "^[a-z][a-z0-9-]+(\\.[a-z][a-z0-9-]+)*(:[0-9]+)?$";
+        String hostnameRegex = "^[a-z0-9-]+(\\.[a-z0-9-]+)*(:[0-9]+)?$";
         String ipRegex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(:[0-9]+)?$";
 
         if (!connectionUriDomain.matches(hostnameRegex) && !connectionUriDomain.matches(ipRegex)) {
