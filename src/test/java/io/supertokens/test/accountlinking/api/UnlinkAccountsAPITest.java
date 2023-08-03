a7776b96e5e8673247fbbf73fa3f1eb877cf8982
rishabhpoddar <rishabh.poddar@gmail.com>
2023-08-03 12:59:23 +0530

    adds more tests

--- src/test/java/io/supertokens/test/accountlinking/api/UnlinkAccountsAPITest.java
+++ src/test/java/io/supertokens/test/accountlinking/api/UnlinkAccountsAPITest.java
@@ -14,32 +14,34 @@
  *    under the License.
  */
 
-package io.supertokens.test.accountlinking;
+package io.supertokens.test.accountlinking.api;
 
 import com.google.gson.JsonObject;
 import io.supertokens.ProcessState;
 import io.supertokens.authRecipe.AuthRecipe;
-import io.supertokens.authRecipe.exception.InputUserIdIsNotAPrimaryUserException;
 import io.supertokens.emailpassword.EmailPassword;
 import io.supertokens.featureflag.EE_FEATURES;
 import io.supertokens.featureflag.FeatureFlagTestContent;
 import io.supertokens.pluginInterface.STORAGE_TYPE;
 import io.supertokens.pluginInterface.authRecipe.AuthRecipeUserInfo;
-import io.supertokens.pluginInterface.emailpassword.exceptions.UnknownUserIdException;
 import io.supertokens.session.Session;
 import io.supertokens.storageLayer.StorageLayer;
 import io.supertokens.test.TestingProcessManager;
 import io.supertokens.test.Utils;
+import io.supertokens.test.httpRequest.HttpRequestForTesting;
+import io.supertokens.test.httpRequest.HttpResponseException;
+import io.supertokens.useridmapping.UserIdMapping;
+import io.supertokens.webserver.WebserverAPI;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 
-public class UnlinkAccountsTest {
+public class UnlinkAccountsAPITest {
     @Rule
     public TestRule watchman = Utils.getOnFailure();
 
@@ -83,8 +85,18 @@ public class UnlinkAccountsTest {
         String[] sessions = Session.getAllNonExpiredSessionHandlesForUser(process.main, user2.id);
         assert (sessions.length == 1);
 
-        boolean didDelete = AuthRecipe.unlinkAccounts(process.main, user2.id);
-        assert (!didDelete);
+        {
+            JsonObject params = new JsonObject();
+            params.addProperty("recipeUserId", user2.id);
+            JsonObject response = HttpRequestForTesting.sendJsonPOSTRequest(process.getProcess(), "",
+                    "http://localhost:3567/recipe/accountlinking/user/unlink", params, 1000, 1000, null,
+                    WebserverAPI.getLatestCDIVersion().get(), "");
+            assertEquals(3, response.entrySet().size());
+            assertEquals("OK", response.get("status").getAsString());
+            assertFalse(response.get("wasRecipeUserDeleted").getAsBoolean());
+            assertTrue(response.get("wasLinked").getAsBoolean());
+        }
+
 
         AuthRecipeUserInfo refetchUser2 = AuthRecipe.getUserById(process.main, user2.id);
         assert (!refetchUser2.isPrimaryUser);
@@ -101,6 +113,94 @@ public class UnlinkAccountsTest {
         // cause linkAccounts revokes sessions for the recipe user ID
         sessions = Session.getAllNonExpiredSessionHandlesForUser(process.main, user2.id);
         assert (sessions.length == 0);
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+
+    @Test
+    public void unlinkAccountBadRequest() throws Exception {
+        String[] args = {"../"};
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args, false);
+        FeatureFlagTestContent.getInstance(process.getProcess())
+                .setKeyValue(FeatureFlagTestContent.ENABLED_FEATURES, new EE_FEATURES[]{
+                        EE_FEATURES.ACCOUNT_LINKING, EE_FEATURES.MULTI_TENANCY});
+        process.startProcess();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+
+        try {
+            JsonObject params = new JsonObject();
+            HttpRequestForTesting.sendJsonPOSTRequest(process.getProcess(), "",
+                    "http://localhost:3567/recipe/accountlinking/user/unlink", params, 1000, 1000, null,
+                    WebserverAPI.getLatestCDIVersion().get(), "");
+            assert (false);
+        } catch (HttpResponseException e) {
+            assert (e.statusCode == 400);
+            assert (e.getMessage()
+                    .equals("Http error. Status Code: 400. Message: Field name 'recipeUserId' is invalid in JSON " +
+                            "input"));
+        }
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+    @Test
+    public void unlinkAccountSuccessWithUserIdMapping() throws Exception {
+        String[] args = {"../"};
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args, false);
+        FeatureFlagTestContent.getInstance(process.getProcess())
+                .setKeyValue(FeatureFlagTestContent.ENABLED_FEATURES, new EE_FEATURES[]{
+                        EE_FEATURES.ACCOUNT_LINKING, EE_FEATURES.MULTI_TENANCY});
+        process.startProcess();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+
+        AuthRecipeUserInfo user = EmailPassword.signUp(process.getProcess(), "test@example.com", "password");
+        assert (!user.isPrimaryUser);
+
+        Thread.sleep(50);
+
+        AuthRecipeUserInfo user2 = EmailPassword.signUp(process.getProcess(), "test2@example.com", "password");
+        assert (!user2.isPrimaryUser);
+        UserIdMapping.createUserIdMapping(process.main, user2.id, "e2", null, false);
+
+        AuthRecipe.createPrimaryUser(process.main, user.id);
+
+        AuthRecipe.linkAccounts(process.main, user2.id, user.id);
+
+        {
+            JsonObject params = new JsonObject();
+            params.addProperty("recipeUserId", "e2");
+            JsonObject response = HttpRequestForTesting.sendJsonPOSTRequest(process.getProcess(), "",
+                    "http://localhost:3567/recipe/accountlinking/user/unlink", params, 1000, 1000, null,
+                    WebserverAPI.getLatestCDIVersion().get(), "");
+            assertEquals(3, response.entrySet().size());
+            assertEquals("OK", response.get("status").getAsString());
+            assertFalse(response.get("wasRecipeUserDeleted").getAsBoolean());
+            assertTrue(response.get("wasLinked").getAsBoolean());
+        }
+
+
+        AuthRecipeUserInfo refetchUser2 = AuthRecipe.getUserById(process.main, user2.id);
+        assert (!refetchUser2.isPrimaryUser);
+        assert (refetchUser2.id.equals(user2.id));
+        assert (refetchUser2.loginMethods.length == 1);
+        assert (refetchUser2.loginMethods[0].recipeUserId.equals(user2.id));
+
+        AuthRecipeUserInfo refetchUser = AuthRecipe.getUserById(process.main, user.id);
+        assert (!refetchUser2.equals(refetchUser));
+        assert (refetchUser.isPrimaryUser);
+        assert (refetchUser.loginMethods.length == 1);
+        assert (refetchUser.loginMethods[0].recipeUserId.equals(user.id));
 
         process.kill();
         assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
@@ -123,11 +223,16 @@ public class UnlinkAccountsTest {
         AuthRecipeUserInfo user = EmailPassword.signUp(process.getProcess(), "test@example.com", "password");
         assert (!user.isPrimaryUser);
 
-        try {
-            AuthRecipe.unlinkAccounts(process.main, user.id);
-            assert (false);
-        } catch (InputUserIdIsNotAPrimaryUserException e) {
-            assert (e.userId.equals(user.id));
+        {
+            JsonObject params = new JsonObject();
+            params.addProperty("recipeUserId", user.id);
+            JsonObject response = HttpRequestForTesting.sendJsonPOSTRequest(process.getProcess(), "",
+                    "http://localhost:3567/recipe/accountlinking/user/unlink", params, 1000, 1000, null,
+                    WebserverAPI.getLatestCDIVersion().get(), "");
+            assertEquals(3, response.entrySet().size());
+            assertEquals("OK", response.get("status").getAsString());
+            assertFalse(response.get("wasRecipeUserDeleted").getAsBoolean());
+            assertFalse(response.get("wasLinked").getAsBoolean());
         }
 
 
@@ -150,50 +255,17 @@ public class UnlinkAccountsTest {
         }
 
         try {
-            AuthRecipe.unlinkAccounts(process.main, "random");
+            JsonObject params = new JsonObject();
+            params.addProperty("recipeUserId", "random");
+            HttpRequestForTesting.sendJsonPOSTRequest(process.getProcess(), "",
+                    "http://localhost:3567/recipe/accountlinking/user/unlink", params, 1000, 1000, null,
+                    WebserverAPI.getLatestCDIVersion().get(), "");
             assert (false);
-        } catch (UnknownUserIdException e) {
+        } catch (HttpResponseException e) {
+            assert (e.statusCode == 400);
+            assert (e.getMessage().equals("Http error. Status Code: 400. Message: Unknown user ID provided"));
         }
 
-
-        process.kill();
-        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
-    }
-
-    @Test
-    public void unlinkAccountWithPrimaryUserBecomesRecipeUser() throws Exception {
-        String[] args = {"../"};
-        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args, false);
-        FeatureFlagTestContent.getInstance(process.getProcess())
-                .setKeyValue(FeatureFlagTestContent.ENABLED_FEATURES, new EE_FEATURES[]{
-                        EE_FEATURES.ACCOUNT_LINKING, EE_FEATURES.MULTI_TENANCY});
-        process.startProcess();
-        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
-
-        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
-            return;
-        }
-
-        AuthRecipeUserInfo user = EmailPassword.signUp(process.getProcess(), "test@example.com", "password");
-        assert (!user.isPrimaryUser);
-
-        AuthRecipe.createPrimaryUser(process.main, user.id);
-
-        Session.createNewSession(process.main, user.id, new JsonObject(), new JsonObject());
-        String[] sessions = Session.getAllNonExpiredSessionHandlesForUser(process.main, user.id);
-        assert (sessions.length == 1);
-
-        boolean didDelete = AuthRecipe.unlinkAccounts(process.main, user.id);
-        assert (!didDelete);
-
-        AuthRecipeUserInfo refetchUser = AuthRecipe.getUserById(process.main, user.id);
-        assert (!refetchUser.isPrimaryUser);
-        assert (refetchUser.loginMethods.length == 1);
-        assert (refetchUser.loginMethods[0].recipeUserId.equals(user.id));
-
-        // cause linkAccounts revokes sessions for the recipe user ID
-        sessions = Session.getAllNonExpiredSessionHandlesForUser(process.main, user.id);
-        assert (sessions.length == 0);
 
         process.kill();
         assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
@@ -225,25 +297,17 @@ public class UnlinkAccountsTest {
 
         AuthRecipe.linkAccounts(process.main, user2.id, user.id);
 
-        Session.createNewSession(process.main, user.id, new JsonObject(), new JsonObject());
-        String[] sessions = Session.getAllNonExpiredSessionHandlesForUser(process.main, user.id);
-        assert (sessions.length == 1);
-
-        boolean didDelete = AuthRecipe.unlinkAccounts(process.main, user.id);
-        assert (didDelete);
-
-        AuthRecipeUserInfo refetchUser2 = AuthRecipe.getUserById(process.main, user2.id);
-        assert (refetchUser2.isPrimaryUser);
-        assert (refetchUser2.id.equals(user.id));
-        assert (refetchUser2.loginMethods.length == 1);
-        assert (refetchUser2.loginMethods[0].recipeUserId.equals(user2.id));
-
-        AuthRecipeUserInfo refetchUser = AuthRecipe.getUserById(process.main, user.id);
-        assert (refetchUser2.equals(refetchUser));
-
-        // cause linkAccounts revokes sessions for the recipe user ID
-        sessions = Session.getAllNonExpiredSessionHandlesForUser(process.main, user.id);
-        assert (sessions.length == 0);
+        {
+            JsonObject params = new JsonObject();
+            params.addProperty("recipeUserId", user.id);
+            JsonObject response = HttpRequestForTesting.sendJsonPOSTRequest(process.getProcess(), "",
+                    "http://localhost:3567/recipe/accountlinking/user/unlink", params, 1000, 1000, null,
+                    WebserverAPI.getLatestCDIVersion().get(), "");
+            assertEquals(3, response.entrySet().size());
+            assertEquals("OK", response.get("status").getAsString());
+            assertTrue(response.get("wasRecipeUserDeleted").getAsBoolean());
+            assertTrue(response.get("wasLinked").getAsBoolean());
+        }
 
         process.kill();
         assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
