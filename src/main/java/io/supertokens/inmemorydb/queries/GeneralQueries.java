4e345fb0d1fc45675156e3b5f69cbd933913eec6
Tanish Azad <tanishazad03@gmail.com>
2023-10-16 12:46:55 +0530

    updated copyright

--- src/main/java/io/supertokens/inmemorydb/queries/GeneralQueries.java
+++ src/main/java/io/supertokens/inmemorydb/queries/GeneralQueries.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright (c) 2020, VRAI Labs and/or its affiliates. All rights reserved.
+ *    Copyright (c) 2023, SuperTokens, Inc. All rights reserved.
  *
  *    This software is licensed under the Apache License, Version 2.0 (the
  *    "License") as published by the Apache Software Foundation.
