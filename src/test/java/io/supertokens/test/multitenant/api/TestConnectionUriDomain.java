b6f6ac00d3c62b1e6a6612baa72769ae857067cf
Sattvik Chakravarthy <sattvik@gmail.com>
2023-10-26 13:13:16 +0530

    fix: pr comments

--- src/test/java/io/supertokens/test/multitenant/api/TestConnectionUriDomain.java
+++ src/test/java/io/supertokens/test/multitenant/api/TestConnectionUriDomain.java
@@ -752,4 +752,42 @@ public class TestConnectionUriDomain {
                 process.getProcess(), SemVer.v4_1);
         assertNull(tenant.get("defaultRequiredFactorIds"));
     }
+
+    @Test
+    public void testDuplicateValuesInFirstFactorsAndDefaultRequiredFactorIds() throws Exception {
+        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+
+        JsonObject config = new JsonObject();
+        StorageLayer.getBaseStorage(process.getProcess()).modifyConfigToAddANewUserPoolForTesting(config, 1);
+
+        String[] factors = new String[]{"duplicate", "emailpassword", "duplicate", "custom"};
+        try {
+            TestMultitenancyAPIHelper.createConnectionUriDomain(
+                    process.getProcess(),
+                    new TenantIdentifier(null, null, null),
+                    "127.0.0.1", null, null, null,
+                    null, true, factors, false, null,
+                    config, SemVer.v4_1);
+            fail();
+        } catch (HttpResponseException e) {
+            assertEquals(400, e.statusCode);
+            assertEquals("Http error. Status Code: 400. Message: firstFactors input should not contain duplicate values", e.getMessage());
+        }
+
+        try {
+            TestMultitenancyAPIHelper.createConnectionUriDomain(
+                    process.getProcess(),
+                    new TenantIdentifier(null, null, null),
+                    "127.0.0.1", null, null, null,
+                    null, false, null, true, factors,
+                    config, SemVer.v4_1);
+            fail();
+        } catch (HttpResponseException e) {
+            assertEquals(400, e.statusCode);
+            assertEquals("Http error. Status Code: 400. Message: defaultRequiredFactorIds input should not contain duplicate values", e.getMessage());
+        }
+
+    }
 }
