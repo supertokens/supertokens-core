59c794e803cafc79c2448052e73e805d05475ea9
Sattvik Chakravarthy <sattvik@gmail.com>
2023-10-26 12:34:57 +0530

    fix: tests

--- src/test/java/io/supertokens/test/multitenant/TestAppData.java
+++ src/test/java/io/supertokens/test/multitenant/TestAppData.java
@@ -103,7 +103,7 @@ public class TestAppData {
             return;
         }
 
-        String[] tablesToIgnore = new String[]{"tenant_thirdparty_provider_clients", "tenant_thirdparty_providers", "first_factors", "default_required_factor_ids"};
+        String[] tablesToIgnore = new String[]{"tenant_thirdparty_provider_clients", "tenant_thirdparty_providers", "tenant_first_factors", "tenant_default_required_factor_ids"};
 
         TenantIdentifier app = new TenantIdentifier(null, "a1", null);
 
