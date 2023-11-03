4dff2c3e710769c5b1530289432845fad3983a0d
Sattvik Chakravarthy <sattvik@gmail.com>
2023-11-03 16:12:23 +0530

    fix: requests stats

---
+++ src/main/java/io/supertokens/webserver/RequestStats.java
@@ -0,0 +1,143 @@
+/*
+ *    Copyright (c) 2023, VRAI Labs and/or its affiliates. All rights reserved.
+ *
+ *    This software is licensed under the Apache License, Version 2.0 (the
+ *    "License") as published by the Apache Software Foundation.
+ *
+ *    You may not use this file except in compliance with the License. You may
+ *    obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *    License for the specific language governing permissions and limitations
+ *    under the License.
+ */
+
+package io.supertokens.webserver;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import io.supertokens.Main;
+import io.supertokens.ResourceDistributor;
+import io.supertokens.multitenancy.Multitenancy;
+import io.supertokens.pluginInterface.multitenancy.AppIdentifier;
+import io.supertokens.pluginInterface.multitenancy.exceptions.TenantOrAppNotFoundException;
+
+public class RequestStats extends ResourceDistributor.SingletonResource {
+    public static final String RESOURCE_KEY = "io.supertokens.webserver.RequestStats";
+
+    private final int MAX_MINUTES = 24 * 60;
+
+    private long currentMinute; // current minute since epoch
+    private final int[] currentMinuteRequestCounts; // array of 60 items representing number of requests at each second in the current minute
+
+    // The 2 arrays below contains stats for a day for every minute
+    // the array is stored in such a way that array[currentMinute % MAX_MINUTES] contains the stats for a day ago
+    // until array[(currentMinute - 1) % MAX_MINUTES] which contains the stats for the last minute, circling around
+    // from end of array to the beginning
+    // for e.g. if currentMinute % MAX_MINUTES = 250,
+    // then array[250] contains stats for now - 1440 minutes
+    // array[251] contains stats for now - 1439 minutes
+    // ...
+    // array[1439] contains stats for now - 1191 minutes
+    // array[0] contains stats for now - 1190 minutes
+    // array[1] contains stats for now - 1189 minutes
+    // ...
+    // array[249] contains stats for now - 1 minute
+    private final double[] averageRequestsPerSecond;
+    private final int[] peakRequestsPerSecond;
+
+    private RequestStats() {
+        currentMinute = System.currentTimeMillis() / 60000;
+        currentMinuteRequestCounts = new int[60];
+
+        averageRequestsPerSecond = new double[MAX_MINUTES];
+        peakRequestsPerSecond = new int[MAX_MINUTES];
+        for (int i = 0; i < MAX_MINUTES; i++) {
+            averageRequestsPerSecond[i] = -1;
+            peakRequestsPerSecond[i] = -1;
+        }
+    }
+
+    private void checkAndUpdateMinute(long currentSecond) {
+        if (currentSecond / 60 == currentMinute) {
+            return; // stats update not required
+        }
+
+        int sum = 0;
+        int max = 0;
+        for (int i = 0; i < 60; i++) {
+            sum += currentMinuteRequestCounts[i];
+            max = Math.max(max, currentMinuteRequestCounts[i]);
+        }
+
+        averageRequestsPerSecond[(int) (currentMinute % MAX_MINUTES)] = sum / 60.0;
+        peakRequestsPerSecond[(int) (currentMinute % MAX_MINUTES)] = max;
+
+        // fill zeros for passed minutes
+        for (long i = currentMinute + 1; i < currentSecond / 60; i++) {
+            averageRequestsPerSecond[(int) (i % MAX_MINUTES)] = 0;
+            peakRequestsPerSecond[(int) (i % MAX_MINUTES)] = 0;
+        }
+
+        currentMinute = currentSecond / 60;
+        for (int i = 0; i < 60; i++) {
+            currentMinuteRequestCounts[i] = 0;
+        }
+    }
+
+    private void updateCounts(long currentSecond) {
+        currentMinuteRequestCounts[(int) (currentSecond % 60)]++;
+    }
+
+    public static RequestStats getInstance(Main main, AppIdentifier appIdentifier) throws TenantOrAppNotFoundException {
+        try {
+            return (RequestStats) main.getResourceDistributor()
+                    .getResource(appIdentifier.getAsPublicTenantIdentifier(), RESOURCE_KEY);
+        } catch (TenantOrAppNotFoundException e) {
+            // appIdentifier parameter is coming from the API request and hence we need to check if the app exists
+            // before creating a resource for it, otherwise someone could fill up memory by making requests for apps
+            // that don't exist.
+            // The other resources are created during init or while refreshing tenants from the db, so we don't need
+            // this kind of pattern for those resources.
+            if (Multitenancy.getTenantInfo(main, appIdentifier.getAsPublicTenantIdentifier()) == null) {
+                throw e;
+            }
+            return (RequestStats) main.getResourceDistributor()
+                    .setResource(appIdentifier.getAsPublicTenantIdentifier(), RESOURCE_KEY, new RequestStats());
+        }
+    }
+
+    public void updateRequestStats() {
+        this.updateRequestStats(true);
+    }
+
+    synchronized private void updateRequestStats(boolean updateCounts) {
+        long now = System.currentTimeMillis() / 1000;
+        this.checkAndUpdateMinute(now);
+        if (updateCounts) { this.updateCounts(now); }
+    }
+
+    public JsonObject getStats() {
+        this.updateRequestStats(false);
+
+        JsonArray avgRps = new JsonArray();
+        JsonArray peakRps = new JsonArray();
+
+        long atMinute = System.currentTimeMillis() / 60000;
+
+        int offset = (int) (atMinute % MAX_MINUTES);
+        for (int i = 0; i < MAX_MINUTES; i++) {
+            avgRps.add(new JsonPrimitive(this.averageRequestsPerSecond[(i + offset) % MAX_MINUTES]));
+            peakRps.add(new JsonPrimitive(this.peakRequestsPerSecond[(i + offset) % MAX_MINUTES]));
+        }
+
+        JsonObject result = new JsonObject();
+        result.addProperty("atMinute", atMinute);
+        result.add("averageRequestsPerSecond", avgRps);
+        result.add("peakRequestsPerSecond", peakRps);
+        return result;
+    }
+}
---
+++ src/test/java/io/supertokens/test/RequestStatsTest.java
@@ -0,0 +1,289 @@
+/*
+ *    Copyright (c) 2023, VRAI Labs and/or its affiliates. All rights reserved.
+ *
+ *    This software is licensed under the Apache License, Version 2.0 (the
+ *    "License") as published by the Apache Software Foundation.
+ *
+ *    You may not use this file except in compliance with the License. You may
+ *    obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *    License for the specific language governing permissions and limitations
+ *    under the License.
+ */
+
+package io.supertokens.test;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import io.supertokens.ProcessState;
+import io.supertokens.featureflag.EE_FEATURES;
+import io.supertokens.featureflag.FeatureFlagTestContent;
+import io.supertokens.multitenancy.Multitenancy;
+import io.supertokens.pluginInterface.STORAGE_TYPE;
+import io.supertokens.pluginInterface.multitenancy.*;
+import io.supertokens.pluginInterface.multitenancy.exceptions.TenantOrAppNotFoundException;
+import io.supertokens.storageLayer.StorageLayer;
+import io.supertokens.test.httpRequest.HttpRequestForTesting;
+import io.supertokens.test.httpRequest.HttpResponseException;
+import io.supertokens.test.multitenant.api.TestMultitenancyAPIHelper;
+import io.supertokens.webserver.RequestStats;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+public class RequestStatsTest {
+    @Rule
+    public TestRule watchman = Utils.getOnFailure();
+
+    @AfterClass
+    public static void afterTesting() {
+        Utils.afterTesting();
+    }
+
+    @Before
+    public void beforeEach() {
+        Utils.reset();
+    }
+
+    @Test
+    public void testLastMinuteStats() throws Exception {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args);
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+
+        // Wait for a minute to pass
+        Thread.sleep(60000 - (System.currentTimeMillis() % 60000) + 100);
+
+        ExecutorService ex = Executors.newFixedThreadPool(100);
+        int numRequests = 1000;
+        for (int i = 0; i < numRequests; i++) {
+            int finalI = i;
+            ex.execute(() -> {
+                try {
+                    TestMultitenancyAPIHelper.epSignUp(TenantIdentifier.BASE_TENANT, "test" + finalI + "@example.com", "password", process.getProcess());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        ex.shutdown();
+        ex.awaitTermination(45, TimeUnit.SECONDS); // should finish in 45 seconds
+
+        // Wait for a minute to pass
+        Thread.sleep(60000 - (System.currentTimeMillis() % 60000) + 100);
+
+        JsonObject stats = HttpRequestForTesting
+                .sendGETRequest(process.getProcess(), "", "http://localhost:3567/requests/stats", null, 5000,
+                        5000, null, Utils.getCdiVersionStringLatestForTests(), null);
+
+        JsonArray avgRps = stats.get("averageRequestsPerSecond").getAsJsonArray();
+        JsonArray peakRps = stats.get("peakRequestsPerSecond").getAsJsonArray();
+
+        double avg = 10000;
+
+        int count = 0;
+        for (JsonElement e : avgRps) {
+            if (e.getAsDouble() == -1) {
+                count++;
+            } else {
+                assertEquals(numRequests, Math.round(e.getAsDouble() * 60));
+                avg = e.getAsDouble();
+            }
+        }
+        assertEquals(1439, count);
+
+        count = 0;
+        for (JsonElement e : peakRps) {
+            if (e.getAsInt() == -1) {
+                count++;
+            } else {
+                assertTrue(e.getAsInt() > avg);
+            }
+        }
+        assertEquals(1439, count);
+
+        assertEquals(System.currentTimeMillis() / 60000, stats.get("atMinute").getAsLong());
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+    @Test
+    public void testLastMinuteStatsPerApp() throws Exception {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args, false);
+        FeatureFlagTestContent.getInstance(process.getProcess())
+                .setKeyValue(FeatureFlagTestContent.ENABLED_FEATURES, new EE_FEATURES[]{
+                        EE_FEATURES.MULTI_TENANCY});
+        process.startProcess();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+
+        Multitenancy.addNewOrUpdateAppOrTenant(process.getProcess(), new TenantConfig(
+                new TenantIdentifier(null, "a1", null),
+                new EmailPasswordConfig(true),
+                new ThirdPartyConfig(true, null),
+                new PasswordlessConfig(true),
+                new JsonObject()
+        ), false);
+
+        // Wait for a minute to pass
+        Thread.sleep(60000 - (System.currentTimeMillis() % 60000) + 100);
+
+        ExecutorService ex = Executors.newFixedThreadPool(100);
+        int numRequests = 500;
+        for (int i = 0; i < numRequests; i++) {
+            int finalI = i;
+            ex.execute(() -> {
+                try {
+                    TestMultitenancyAPIHelper.epSignUp(TenantIdentifier.BASE_TENANT, "test" + finalI + "@example.com", "password", process.getProcess());
+                } catch (Exception e) {
+                    // ignore
+                }
+                if (finalI < 400) {
+                    try {
+                        TestMultitenancyAPIHelper.epSignUp(new TenantIdentifier(null, "a1", null), "test" + finalI + "@example.com", "password", process.getProcess());
+                    } catch (Exception e) {
+                        // ignore
+                    }
+                }
+            });
+        }
+
+        ex.shutdown();
+        ex.awaitTermination(45, TimeUnit.SECONDS); // should finish in 45 seconds
+
+        // Wait for a minute to pass
+        Thread.sleep(60000 - (System.currentTimeMillis() % 60000) + 100);
+
+        {
+            JsonObject stats = HttpRequestForTesting
+                    .sendGETRequest(process.getProcess(), "", "http://localhost:3567/requests/stats", null, 5000,
+                            5000, null, Utils.getCdiVersionStringLatestForTests(), null);
+
+            JsonArray avgRps = stats.get("averageRequestsPerSecond").getAsJsonArray();
+            JsonArray peakRps = stats.get("peakRequestsPerSecond").getAsJsonArray();
+
+            double avg = 10000;
+
+            int count = 0;
+            for (JsonElement e : avgRps) {
+                if (e.getAsDouble() == -1) {
+                    count++;
+                } else {
+                    assertEquals(numRequests, Math.round(e.getAsDouble() * 60));
+                    avg = e.getAsDouble();
+                }
+            }
+            assertEquals(1439, count);
+
+            count = 0;
+            for (JsonElement e : peakRps) {
+                if (e.getAsInt() == -1) {
+                    count++;
+                } else {
+                    assertTrue(e.getAsInt() > avg);
+                }
+            }
+            assertEquals(1439, count);
+
+            assertEquals(System.currentTimeMillis() / 60000, stats.get("atMinute").getAsLong());
+        }
+
+        {
+            JsonObject stats = HttpRequestForTesting
+                    .sendGETRequest(process.getProcess(), "", "http://localhost:3567/appid-a1/requests/stats", null, 1000,
+                            1000, null, Utils.getCdiVersionStringLatestForTests(), null);
+
+            JsonArray avgRps = stats.get("averageRequestsPerSecond").getAsJsonArray();
+            JsonArray peakRps = stats.get("peakRequestsPerSecond").getAsJsonArray();
+
+            double avg = 10000;
+
+            int count = 0;
+            for (JsonElement e : avgRps) {
+                if (e.getAsDouble() == -1) {
+                    count++;
+                } else {
+                    assertEquals(400, Math.round(e.getAsDouble() * 60));
+                    avg = e.getAsDouble();
+                }
+            }
+            assertEquals(1439, count);
+
+            count = 0;
+            for (JsonElement e : peakRps) {
+                if (e.getAsInt() == -1) {
+                    count++;
+                } else {
+                    assertTrue(e.getAsInt() > avg);
+                }
+            }
+            assertEquals(1439, count);
+
+            assertEquals(System.currentTimeMillis() / 60000, stats.get("atMinute").getAsLong());
+        }
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+    @Test
+    public void testWithNonExistantApp() throws Exception {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args, false);
+        FeatureFlagTestContent.getInstance(process.getProcess())
+                .setKeyValue(FeatureFlagTestContent.ENABLED_FEATURES, new EE_FEATURES[]{
+                        EE_FEATURES.MULTI_TENANCY});
+        process.startProcess();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+
+        try {
+            RequestStats.getInstance(process.getProcess(), new AppIdentifier(null, "a1"));
+            fail();
+        } catch (TenantOrAppNotFoundException e) {
+            // ok
+        }
+
+        try {
+            JsonObject stats = HttpRequestForTesting
+                    .sendGETRequest(process.getProcess(), "", "http://localhost:3567/appid-a1/requests/stats", null, 1000,
+                            1000, null, Utils.getCdiVersionStringLatestForTests(), null);
+            fail();
+        } catch (HttpResponseException e) {
+            assertEquals(400, e.statusCode);
+            assertEquals("Http error. Status Code: 400. Message: AppId or tenantId not found => Tenant with the following connectionURIDomain, appId and tenantId combination not found: (, a1, public)", e.getMessage());
+        }
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+}
