ea482d9114b125c4ac020c53e53fdb97edfa1e4b
Mihaly Lengyel <mihaly@lengyel.tech>
2024-10-29 11:53:52 +0100

    test: try and improve flaky tests

--- .circleci/doTests.sh
+++ .circleci/doTests.sh
@@ -171,10 +171,15 @@ do
 
           if [ -d ~/junit ]
           then
+            echo "Copying output from core"
+            ls ~/supertokens-root/supertokens-core/build/test-results/test/*.xml
             cp ~/supertokens-root/supertokens-core/build/test-results/test/*.xml ~/junit/
 
             if [[ $pluginToTest -ne "sqlite" ]]
             then
+              echo "Copying output from plugin"
+              echo "Using path: ~/supertokens-root/supertokens-$pluginToTest-plugin/build/test-results/test/*.xml"
+              ls "~/supertokens-root/supertokens-$pluginToTest-plugin/build/test-results/test/*.xml"
               cp "~/supertokens-root/supertokens-$pluginToTest-plugin/build/test-results/test/*.xml" ~/junit/
             fi
           fi
