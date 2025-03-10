49accb65e54741ac21332dae8d4100659e8ff3a5
Sattvik Chakravarthy <sattvik@gmail.com>
2025-03-10 17:19:52 +0530

    test: sign in options

--- src/test/java/io/supertokens/test/webauthn/api/TestSignInOptionsAPI_5_3.java
+++ src/test/java/io/supertokens/test/webauthn/api/TestSignInOptionsAPI_5_3.java
@@ -1,12 +1,24 @@
 package io.supertokens.test.webauthn.api;
 
+import com.google.gson.JsonObject;
+import io.supertokens.ProcessState;
+import io.supertokens.pluginInterface.STORAGE_TYPE;
+import io.supertokens.storageLayer.StorageLayer;
+import io.supertokens.test.TestingProcessManager;
 import io.supertokens.test.Utils;
+import io.supertokens.test.httpRequest.HttpRequestForTesting;
+import io.supertokens.test.httpRequest.HttpResponseException;
+import io.supertokens.utils.SemVer;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.TestRule;
 
-class TestSignInOptionsAPI_5_3 {
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+public class TestSignInOptionsAPI_5_3 {
     @Rule
     public TestRule watchman = Utils.getOnFailure();
 
@@ -20,4 +32,108 @@ class TestSignInOptionsAPI_5_3 {
         Utils.reset();
     }
 
+    @Test
+    public void testInvalidInput() throws Exception {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args);
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+
+        JsonObject req = new JsonObject();
+        try {
+            JsonObject resp = HttpRequestForTesting.sendJsonPOSTRequest(process.getProcess(), "",
+                    "http://localhost:3567/recipe/webauthn/options/signin", req, 1000, 1000, null,
+                    SemVer.v5_3.get(), "webauthn");
+            fail();
+        } catch (HttpResponseException e) {
+            assertEquals(400, e.statusCode);
+            assertEquals("Http error. Status Code: 400. Message: Field name 'relyingPartyId' is invalid in JSON input", e.getMessage());
+        }
+
+        req.addProperty("relyingPartyId", "example.com");
+        try {
+            JsonObject resp = HttpRequestForTesting.sendJsonPOSTRequest(process.getProcess(), "",
+                    "http://localhost:3567/recipe/webauthn/options/signin", req, 1000, 1000, null,
+                    SemVer.v5_3.get(), "webauthn");
+            fail();
+        } catch (HttpResponseException e) {
+            assertEquals(400, e.statusCode);
+            assertEquals("Http error. Status Code: 400. Message: Field name 'relyingPartyName' is invalid in JSON input", e.getMessage());
+        }
+
+        req.addProperty("relyingPartyName", "Example");
+        try {
+            JsonObject resp = HttpRequestForTesting.sendJsonPOSTRequest(process.getProcess(), "",
+                    "http://localhost:3567/recipe/webauthn/options/signin", req, 1000, 1000, null,
+                    SemVer.v5_3.get(), "webauthn");
+            fail();
+        } catch (HttpResponseException e) {
+            assertEquals(400, e.statusCode);
+            assertEquals("Http error. Status Code: 400. Message: Field name 'origin' is invalid in JSON input", e.getMessage());
+        }
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+    @Test
+    public void testOptionalFields() throws Exception {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args);
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        if (StorageLayer.getStorage(process.getProcess()).getType() != STORAGE_TYPE.SQL) {
+            return;
+        }
+        JsonObject req = new JsonObject();
+        req.addProperty("relyingPartyId", "example.com");
+        req.addProperty("relyingPartyName", "Example");
+        req.addProperty("origin", "http://example.com");
+
+        // timeout
+        try {
+            JsonObject resp = HttpRequestForTesting.sendJsonPOSTRequest(process.getProcess(), "",
+                    "http://localhost:3567/recipe/webauthn/options/signin", req, 1000, 1000, null,
+                    SemVer.v5_3.get(), "webauthn");
+            checkResponseStructure(resp);
+
+            assertEquals(6000, resp.get("timeout").getAsInt());
+        } catch (HttpResponseException e) {
+            fail(e.getMessage());
+        }
+
+        req.addProperty("timeout", 8000);
+        try {
+            JsonObject resp = HttpRequestForTesting.sendJsonPOSTRequest(process.getProcess(), "",
+                    "http://localhost:3567/recipe/webauthn/options/signin", req, 1000, 1000, null,
+                    SemVer.v5_3.get(), "webauthn");
+            checkResponseStructure(resp);
+
+            assertEquals(8000, resp.get("timeout").getAsInt());
+        } catch (HttpResponseException e) {
+            fail(e.getMessage());
+        }
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+    private void checkResponseStructure(JsonObject resp) throws Exception {
+        assertEquals("OK", resp.get("status").getAsString());
+        assertEquals(9, resp.entrySet().size());
+
+        assertTrue(resp.has("webauthnGeneratedOptionsId"));
+        assertTrue(resp.has("relyingPartyId"));
+        assertTrue(resp.has("challenge"));
+        assertTrue(resp.has("timeout"));
+        assertTrue(resp.has("userVerification"));
+        assertTrue(resp.has("userPresence"));
+        assertTrue(resp.has("createdAt"));
+        assertTrue(resp.has("expiresAt"));
+    }
 }
