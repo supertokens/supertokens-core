5809cef4fc1cd1240353e421a818bc2f2b5fceb4
Sattvik <sattvik@Sattviks-MacBook-Pro.local>
2025-03-27 11:34:06 +0530

    fix: flaky

--- src/test/java/io/supertokens/test/Utils.java
+++ src/test/java/io/supertokens/test/Utils.java
@@ -35,6 +35,7 @@ import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.junit.rules.TestRule;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 import org.mockito.Mockito;
 
 import java.io.*;
@@ -176,6 +177,38 @@ public abstract class Utils extends Mockito {
             @Override
             protected void failed(Throwable e, Description description) {
                 System.out.println(byteArrayOutputStream.toString(StandardCharsets.UTF_8));
+            }
+        };
+    }
+
+    public static TestRule retryFlakyTest() {
+        return new TestRule() {
+            private final int retryCount = 3;
+
+            public Statement apply(Statement base, Description description) {
+                return statement(base, description);
+            }
+
+            private Statement statement(final Statement base, final Description description) {
+                return new Statement() {
+                    @Override
+                    public void evaluate() throws Throwable {
+                        Throwable caughtThrowable = null;
+
+                        // implement retry logic here
+                        for (int i = 0; i < retryCount; i++) {
+                            try {
+                                base.evaluate();
+                                return;
+                            } catch (Throwable t) {
+                                caughtThrowable = t;
+                                System.err.println(description.getDisplayName() + ": run " + (i+1) + " failed");
+                            }
+                        }
+                        System.err.println(description.getDisplayName() + ": giving up after " + retryCount + " failures");
+                        throw caughtThrowable;
+                    }
+                };
             }
         };
     }
