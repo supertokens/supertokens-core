856acc19c877e397e7a01c778efe87fe30a292ff
Sattvik <sattvik@Sattviks-MacBook-Pro.local>
2025-04-04 09:09:39 +0530

    fix: tests

--- src/test/java/io/supertokens/test/emailpassword/api/MultitenantAPITest.java
+++ src/test/java/io/supertokens/test/emailpassword/api/MultitenantAPITest.java
@@ -39,10 +39,8 @@ import io.supertokens.test.httpRequest.HttpRequestForTesting;
 import io.supertokens.test.httpRequest.HttpResponseException;
 import io.supertokens.thirdparty.InvalidProviderConfigException;
 import io.supertokens.utils.SemVer;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.rules.TestRule;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -53,6 +51,9 @@ import static org.junit.Assert.*;
 public class MultitenantAPITest {
     TestingProcessManager.TestingProcess process;
     TenantIdentifier t1, t2, t3;
+
+    @Rule
+    public TestRule retryFlaky = Utils.retryFlakyTest();
 
     @AfterClass
     public static void afterTesting() {
