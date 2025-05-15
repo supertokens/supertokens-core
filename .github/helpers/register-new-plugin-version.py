b71264b18eb1fbbbe5c47905b3f4cc0c597dbd08
Sattvik Chakravarthy <sattvik@gmail.com>
2025-05-15 08:02:11 +0530

    fix: register new plugin script

--- .github/helpers/register-new-plugin-version.py
+++ .github/helpers/register-new-plugin-version.py
@@ -1,7 +1,9 @@
 import json
 import os
+import subprocess
 
 import http.client
+
 
 def register_plugin_version(supertokens_api_key, plugin_version, plugin_interface_array, plugin_name):
     print("Plugin Version: ", plugin_version)
@@ -28,6 +30,7 @@ def register_plugin_version(supertokens_api_key, plugin_version, plugin_interfac
 
     if response.status != 200:
         print(f"failed plugin PUT API status code: {response.status}. Exiting!")
+        print(f"response: {str(response.read())}")
         exit(1)
 
     conn.close()
@@ -43,6 +46,19 @@ plugin_version = read_plugin_version()
 
 with open('pluginInterfaceSupported.json', 'r') as fd:
 	plugin_interface_array = json.load(fd)['versions']
+
+def check_if_tag_exists(tag):
+    try:
+        result = subprocess.run(['git', 'tag', '-l', tag], capture_output=True, text=True)
+        return tag in result.stdout
+    except subprocess.CalledProcessError:
+        print(f"Error checking for tag {tag}")
+        return False
+
+dev_tag = f"dev-v{plugin_version}"
+if not check_if_tag_exists(dev_tag):
+    print(f"Tag {dev_tag} does not exist. Exiting!")
+    exit(0)
 
 register_plugin_version(
 	supertokens_api_key=os.environ.get("SUPERTOKENS_API_KEY"),
