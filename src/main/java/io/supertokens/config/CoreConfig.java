9ff5c134472f24d5041ca17ed224ce7c968ca4ec
tamassoltesz <tamas@supertokens.com>
2025-08-15 09:05:16 +0200

    fix: add missing coreconfig description

--- src/main/java/io/supertokens/config/CoreConfig.java
+++ src/main/java/io/supertokens/config/CoreConfig.java
@@ -307,6 +307,10 @@ public class CoreConfig {
 
     @ConfigYamlOnly
     @JsonProperty
+    @ConfigDescription(
+            "The URL of the OpenTelemetry collector to which the core will send telemetry data. " +
+                    "This should be in the format http://<host>:<port> or https://<host>:<port>. (Default: " +
+                    "http://localhost:4317)")
     private String otel_collector_connection_uri = "http://localhost:4317";
 
     public static Set<String> getValidFields() {
